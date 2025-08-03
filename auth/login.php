ec00fd1ea69c13749d5da28918771d7accd39b64
NandoG1 <fernandogunawan291105@gmail.com>
2025-08-03 12:31:25 +0700

    hh

--- auth/login.php
+++ auth/login.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     $_SESSION['user_id'] = $user['ID'];
                     $_SESSION['username'] = $user['Username'];
                     $_SESSION['email'] = $user['Email'];
-                    header('Location: ../main/dashboard.php');
+                    header('Location: ../user/nitro/nitro.php');
                     exit();
                 } else {
                     $error = 'Invalid email or password.';
